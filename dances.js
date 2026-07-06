cbf05c4a05a544d218425d6a40a8cb541bb1bccd
WendyDee65 <wxm3sax@gmail.com>
2026-07-05 17:40:07 -0700

    Step 1 and 2 of redoing the list of dances

---
+++ dances.js
@@ -0,0 +1,30 @@
+const dances = [
+  {
+    name: "A Bar Song Shuffle",
+    level: "Beginner",
+    stepsheet: "",
+    demo: "",
+    tutorial: ""
+  },
+  {
+    name: "Boots On The Ground",
+    level: "Beginner+",
+    stepsheet: "",
+    demo: "",
+    tutorial: ""
+  },
+  {
+    name: "Copperhead Stomp",
+    level: "Improver",
+    stepsheet: "",
+    demo: "",
+    tutorial: ""
+  },
+  {
+    name: "Desert Rose Waltz",
+    level: "Beginner",
+    stepsheet: "",
+    demo: "",
+    tutorial: ""
+  }
+];
