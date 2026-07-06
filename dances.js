bf38a87ad727fb64297abdd45d790a827390ef56
WendyDee65 <wxm3sax@gmail.com>
2026-07-06 09:12:32 -0700

    A dances added

--- dances.js
+++ dances.js
@@ -1,29 +1,29 @@
 const dances = [
   {
-    name: "A Bar Song Shuffle",
-    level: "Beginner",
+    name: "A Bar Song",
+    level: "",
+    stepsheet: "",
+    demo: "https://www.youtube.com/watch?v=6WUNB-EQbsc",
+    tutorial: ""
+  },
+  {
+    name: "AA",
+    level: "",
     stepsheet: "",
     demo: "",
     tutorial: ""
   },
   {
-    name: "Boots On The Ground",
-    level: "Beginner+",
+    name: "Across the Room",
+    level: "",
     stepsheet: "",
-    demo: "",
+    demo: "https://www.youtube.com/watch?v=fq3L9zbd8_A",
     tutorial: ""
   },
   {
-    name: "Copperhead Stomp",
-    level: "Improver",
-    stepsheet: "",
-    demo: "",
-    tutorial: ""
-  },
-  {
-    name: "Desert Rose Waltz",
-    level: "Beginner",
-    stepsheet: "",
+    name: "After Hours",
+    level: "",
+    stepsheet: "https://www.copperknob.co.uk/stepsheets/186672/ab-after-hours",
     demo: "",
     tutorial: ""
   }
