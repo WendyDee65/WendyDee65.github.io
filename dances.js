b745c72540927a2a266226779790a54ce578552a
WendyDee65 <wxm3sax@gmail.com>
2026-07-06 10:25:16 -0700

    1-B

--- dances.js
+++ dances.js
@@ -1,30 +1,187 @@
 const dances = [
+  // ===== 1 =====
+  { name: "1 Margarita", level: "", music: "One Margarita", countWall: "32/4", stepsheet: "", demo: "https://www.youtube.com/watch?v=eM__o29Ngnc&t=24s", tutorial: "" },
+  { name: "11 Beers", level: "", music: "11 Beers", countWall: "32/4", stepsheet: "https://www.copperknob.co.uk/stepsheets/161082/11-beers-ab", demo: "", tutorial: "" },
+  { name: "3 Tequila Floor", level: "", music: "3 Tequila Floor", countWall: "32/4", stepsheet: "", demo: "https://www.youtube.com/watch?v=ReIkczu-Qb0&t=1s", tutorial: "" },
+  { name: "3 Tequila Floor AB", level: "", music: "3 Tquila Floor", countWall: "32/2", stepsheet: "", demo: "https://www.youtube.com/watch?v=fqjcSFnkmOE&t=3s", tutorial: "" },
+  { name: "2 Happy", level: "", music: "Happy Hour", countWall: "32/4", stepsheet: "https://www.copperknob.co.uk/stepsheets/2-happy-ID79396.aspx", demo: "", tutorial: "" },
+
+  // ===== A =====
+  { name: "A Bar Song", level: "", music: "A Bar Song (Tipsy)", countWall: "32/4", stepsheet: "", demo: "https://www.youtube.com/watch?v=6WUNB-EQbsc", tutorial: "" },
+  { name: "AA", level: "", music: "AA", countWall: "32/4", stepsheet: "", demo: "", tutorial: "" },
+  { name: "Across the Room", level: "", music: "You Look Like You Love Me", countWall: "32/4", stepsheet: "", demo: "https://www.youtube.com/watch?v=fq3L9zbd8_A", tutorial: "" },
+  { name: "After Hours", level: "", music: "No demo available", countWall: "32/4", stepsheet: "https://www.copperknob.co.uk/stepsheets/186672/ab-after-hours", demo: "", tutorial: "" },
+
+  // ===== B =====
+    // ===== B =====
+
   {
-    name: "A Bar Song",
+    name: "Baby I Wanna Know",
     level: "",
+    music: "Hey Baby",
+    countWall: "32/4",
     stepsheet: "",
-    demo: "https://www.youtube.com/watch?v=6WUNB-EQbsc",
+    demo: "https://www.youtube.com/watch?v=yAKJ5w6qIfA",
     tutorial: ""
   },
+
   {
-    name: "AA",
+    name: "Bang Bang",
     level: "",
+    music: "(EZ) Bang Bang",
+    countWall: "32/4",
     stepsheet: "",
+    demo: "https://www.youtube.com/watch?v=MZUrcbiX4t0",
+    tutorial: ""
+  },
+
+  {
+    name: "Beach Chair",
+    level: "",
+    music: "Beach Chair",
+    countWall: "32/4",
+    stepsheet: "",
+    demo: "https://www.youtube.com/watch?v=2GdXPg3-R2s",
+    tutorial: ""
+  },
+
+  {
+    name: "Best Of My Boo",
+    level: "",
+    music: "Lil Boo Thang",
+    countWall: "32/4",
+    stepsheet: "",
+    demo: "https://www.youtube.com/watch?t=5s&v=qMH78cC305A",
+    tutorial: ""
+  },
+
+  {
+    name: "Better When I'm Dancin'",
+    level: "",
+    music: "Better When I'm Dancin'",
+    countWall: "32/4",
+    stepsheet: "https://www.copperknob.co.uk/stepsheets/126235/better-when-im-dancin",
     demo: "",
     tutorial: ""
   },
+
   {
-    name: "Across the Room",
+    name: "Black Velvet",
     level: "",
+    music: "Black Velvet",
+    countWall: "32/1",
     stepsheet: "",
-    demo: "https://www.youtube.com/watch?v=fq3L9zbd8_A",
+    demo: "https://www.youtube.com/watch?t=9s&v=pW5gXKOk5yA",
     tutorial: ""
   },
+
   {
-    name: "After Hours",
+    name: "Blurred Lines",
     level: "",
-    stepsheet: "https://www.copperknob.co.uk/stepsheets/186672/ab-after-hours",
-    demo: "",
+    music: "Blurred Lines",
+    countWall: "64/4",
+    stepsheet: "",
+    demo: "https://www.bing.com/videos/search?FORM=VIRE&mid=7EF74DA76C95C6E79BB77EF74DA76C95C6E79BB7&q=blurred+lines+line+dance+video&view=detail",
     tutorial: ""
-  }
+  },
+
+  {
+    name: "Body Like a Back Road",
+    level: "",
+    music: "Body Like a Back Road",
+    countWall: "16/2",
+    stepsheet: "",
+    demo: "https://video.search.yahoo.com/search/video?fr=tightropetb&p=body+like+a+back+road+EZ+line+dance",
+    tutorial: ""
+  },
+
+  {
+    name: "Boots Don't AB EZ",
+    level: "",
+    music: "Boots Don't",
+    countWall: "16/4",
+    stepsheet: "",
+    demo: "https://www.youtube.com/watch?v=o3ndxIacOVg&t=31s",
+    tutorial: ""
+  },
+
+  {
+    name: "Boot Scootin' Boogie",
+    level: "",
+    music: "Boot Scootin’ Boogie",
+    countWall: "46/4",
+    stepsheet: "",
+    demo: "https://www.bing.com/videos/search?FORM=VDRVRV&mid=07D9ADB097988E7D4F5007D9ADB097988E7D4F50&q=youtube+line+dance+videos+watermelon+crawl&view=detail",
+    tutorial: ""
+  },
+
+  {
+    name: "Boots on Bars",
+    level: "",
+    music: "Boots on Bars",
+    countWall: "32/4",
+    stepsheet: "",
+    demo: "https://www.youtube.com/watch?t=1s&v=E4suHJE74R8",
+    tutorial: ""
+  },
+
+  {
+    name: "Boots on the Ground (with clacker fan)",
+    level: "",
+    music: "Boots on the Ground",
+    countWall: "32/4",
+    stepsheet: "",
+    demo: "https://www.youtube.com/watch?v=9NTpCApxBGE",
+    tutorial: ""
+  },
+
+  {
+    name: "Bossa Nova",
+    level: "",
+    music: "Blame It On The Bossa Nova",
+    countWall: "64/4",
+    stepsheet: "",
+    demo: "https://www.youtube.com/watch?v=hadL59L56ZY&feature=youtu.be",
+    tutorial: ""
+  },
+
+  {
+    name: "Bringing It Back",
+    level: "",
+    music: "We're Bringing It Back",
+    countWall: "32/4",
+    stepsheet: "",
+    demo: "https://www.youtube.com/watch?t=39s&v=ZIDnFl67TCM",
+    tutorial: ""
+  },
+
+  {
+    name: "Drinkaby",
+    level: "",
+    music: "Drinkaby",
+    countWall: "48/4",
+    stepsheet: "",
+    demo: "https://www.youtube.com/watch?t=79s&v=_Sm2vEW-9Fs",
+    tutorial: ""
+  },
+
+  {
+    name: "Brown Eyed Girl EZ",
+    level: "",
+    music: "Brown Eyed Girl",
+    countWall: "32/4",
+    stepsheet: "",
+    demo: "https://www.youtube.com/watch?v=5xBs14HRnL4",
+    tutorial: ""
+  },
+
+  {
+    name: "Brown Eyed Girl",
+    level: "",
+    music: "Brown Eyed Girl",
+    countWall: "48/1",
+    stepsheet: "",
+    demo: "https://www.youtube.com/watch?v=ir9h3TaYNWo",
+    tutorial: ""
+  },
 ];
