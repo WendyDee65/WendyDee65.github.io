bd854bdabe72c6ae7e7761ebd4c4a9e98c84536b
WendyDee65 <wxm3sax@gmail.com>
2026-07-07 09:28:11 -0700

    More format and actual details

--- events.js
+++ events.js
@@ -1,27 +1,65 @@
 const events = [
 
   {
-    date: "July 30, 2026",
-    title: "Vine In Line Summer Dance Party",
-    type: "Social Dance",
-    location: "Paso Robles, CA",
-    link: "https://..."
-  },
-
-  {
-    date: "August 14-16, 2026",
-    title: "Central Coast Country Dance Weekend",
+    date: "July 16-17, 2026",
+    title: "Niels Poulsen Workshop (Sold Out)",
     type: "Workshop",
-    location: "Pismo Beach, CA",
-    link: "https://..."
+    location: "Fremont, CA",
+    link: "https://www.amynfriendslinedance.com/copy-of-caldance-2026"
   },
 
   {
-    date: "September 5, 2026",
-    title: "Saturday Night Dance",
-    type: "Local Dance",
-    location: "Atascadero, CA",
-    link: "https://..."
-  }
+    date: "July 23-26, 2026",
+    title: "Palm Springs Dance Madness",
+    type: "Event",
+    location: "Palm Springs, CA",
+    link: "https://palmspringsldm.com/"
+  },
 
+  {
+    date: "October 7-11, 2026",
+    title: "Windy City LineDance Mania",
+    type: "Event",
+    location: "Chicago, IL",
+    link: "https://windycitylinedancemania.com/"
+   },
+
+   {
+    date:  "November  9-14, 2026",
+    title: "Vegas Dance Explosion",
+    type: "Event",
+    location: "Las Vegas, NV",
+    link: "https://www.vegasdanceexplosion.com/"
+  },
+
+  {
+    date: "January 21-24, 2027",
+    title: "Palm Springs Winter Break",
+    type: "Event",
+    location: "Palm Springs, CA",
+    link: "https://www.palmspringswinterbreak.com/"
+  },
+
+    {
+    date: "February, 2027",
+    title: "Cambria Dance Line Dance Extravaganza - Details TBD",
+    type: "Event",
+    location: "Cambria, CA",
+    link: ""
+  },
+
+  {
+    date: "April, 2027",
+    title: "Northwest LineDance Blast - Details TBD",
+    type: "Event",
+    location: "Portland, OR",
+    link: ""
+  },
+    {
+    date: "April, 2027",
+    title: "Heart Of Texas - Details TBD",
+    type: "Event",
+    location: "Dallas, CA",
+    link: ""
+  }
 ];
