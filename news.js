8fcf06f39b61e28fa9993767a02f577d28fc9e33
WendyDee65 <wxm3sax@gmail.com>
2026-07-06 21:57:47 -0700

    Add news.js data enhancement

---
+++ news.js
@@ -0,0 +1,37 @@
+const newsItems = [
+  {
+    month: "July 2026",
+    title: "New Dances This Month",
+    intro: "Here are some of the dances we are teaching or reviewing this month.",
+    dances: [
+      {
+        name: "A Bar Song",
+        level: "Beginner",
+        stepsheet: "",
+        demo: "https://www.youtube.com/watch?v=6WUNB-EQbsc",
+        tutorial: ""
+      },
+      {
+        name: "Dancing Queen",
+        level: "Beginner / Improver",
+        stepsheet: "",
+        demo: "https://www.youtube.com/watch?v=H4yx5x7ieyI",
+        tutorial: ""
+      },
+      {
+        name: "Texas Hold 'Em",
+        level: "Improver",
+        stepsheet: "",
+        demo: "https://www.youtube.com/watch?t=1s&v=ZxO7OPqfT9k",
+        tutorial: ""
+      }
+    ],
+    announcements: [
+
+      "Tuesday classes continue at Centennial Park in Paso Robles.",
+      "  ",
+      "Thursday classes will only be held on July 2, July 9, and July 30.",
+      "No Thursday class on July 16 or July 23.",
+    ]
+  }
+];
