af9d989e8a92e88b6ceba01ed4742b3e22670f6e
WendyDee65 <wxm3sax@gmail.com>
2026-07-08 14:47:00 -0700

    flex & Friday Night

--- events.js
+++ events.js
@@ -2,7 +2,7 @@ const events = [
 
   {
     date: "July 10, 2026",
-    title: "Joy Bonner at the Pour House",
+    title: "Joy Bonner at the Pour House - Friday Night",
     type: "Live Music",
     location: "Paso Robles, CA",
     link: "https://www.my805tix.com/e/bonner/tickets"
