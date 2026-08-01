a29539c5864945f17adb6e02e49c39f35d19e8ef
WendyDee65 <wxm3sax@gmail.com>
2026-07-31 18:17:11 -0700

    Add Steppin Out and copyright

---
+++ steppinOutNews.js
@@ -0,0 +1,71 @@
+const steppinOutNews = {
+  month: "August 2026",
+
+  announcements: [
+  ],
+
+  scheduleUpdates: [
+    "Dates: Thursday August 6, 20 and 27",
+    "Time: 4:30  (warmup) 5:00 - 6:30 PM (lesson)",
+    "Where:  Community Church of Atascadero - Fellowship Hall (Behind the church)",
+    "Address: 5850 Rosario Ave, Atascadero, CA 93422",
+    "Cost $10 drop-in (Cash or Venmo)"
+  ],
+
+  // leave this blank if no alerts:  scheduleAlerts: []
+  scheduleAlerts: [
+    "No Thursday class on August 13."
+  ],
+
+  featuredDances: {
+    title: "Lessons this month",
+
+    dances: [
+        { name: "Siempre", level: "High Beginner", music: "Siempre - Vaqueros and Vagabonds", count: "32", walls: "4", stepsheet: "https://www.copperknob.co.uk/stepsheets/C4CWY8Z/siempre", demo: "https://www.youtube.com/watch?v=cFWD99ln9Kw", tutorial: "https://www.youtube.com/watch?v=Z3tpQoC42AU", info:"" },
+        { name: "Dust Off Of Me", level: "Improver", music: "Dust Off of Me - Dusty Black", count: "48", walls: "2", stepsheet: "https://www.copperknob.co.uk/stepsheets/HNZR5JY/dust-off-of-me", demo: "https://www.youtube.com/watch?v=1Fuh5Wq-GXM", tutorial: "https://www.youtube.com/watch?v=k0YmjbD-ieY", info:"" },
+        { name: "Sweet Portland Dreams", level: "High Intermediate", music: "Sweet Dreams (Are Made of This) - Eurythmics", count: "64", walls: "2", stepsheet: "https://www.copperknob.co.uk/stepsheets/L6P8PGC/sweet-portland-dreams", demo: "https://www.youtube.com/watch?v=pHfX1g9B_ro", tutorial: "https://www.youtube.com/watch?v=f8lYcUY5VBQ", info:"" },
+    ]
+  },
+
+  warmupDances: {
+    title: "Weekly Warm-Up Dances",
+
+    weeks: [
+      {
+        date: "August 6, 2026",
+        dances: [
+          "I'm Free",
+          "One More Silver Dollar",
+          "Ghost Train",
+          "Raised Like That",
+          "No Remorse",
+          "Take A Look At Yourself",
+        ]
+      },
+
+      {
+        date: "August 20, 2026",
+        dances: [
+          "Mamma Maria",
+          "Got to Be Funky",
+          "Crowded Mind",
+          "Take A Look At Yourself",
+          "Ghosted",
+          "Ain't Too Cool"
+        ]
+      },
+
+      {
+        date: "August 27, 2026",
+        dances: [
+          "Back on Texas Time",
+          "Broke",
+          "Cold Heart",
+          "Take A Look At Yourself",
+          "Hello Dolly",
+          "Redneck Angel"
+        ]
+      }
+    ]
+  }
+};
